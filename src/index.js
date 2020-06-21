d74918542ad7b2120e1d66fe5a6b2aeac0c6fae1
nabeel <muhammadnabeel9242@gmail.com>
2020-06-21 16:25:39 +0500

    basic function

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <App name="nabeel" age={21} />
+  </div>,
   document.getElementById('root')
 );
 
